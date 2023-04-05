3934f791eed5c4a6efa8754a6791bbaeb3743db5
weizhen94 <weizhen94@gmail.com>
2023-04-05 10:25:39 +0800

    first commit

---
+++ src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
@@ -0,0 +1,57 @@
+package ibf2022.paf.assessment.server.repositories;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
+
+import ibf2022.paf.assessment.server.models.User;
+
+// TODO: Task 3
+
+@Repository
+public class UserRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public Optional<User> findUserByUsername(String username){
+        // List<User> userList = new LinkedList<>();
+        String findByUsernameSQL = "select * from user where username = ?";
+
+        // BeanPropertyRowMapper<User> rowMapper = new BeanPropertyRowMapper<>(User.class);
+        // List<User> Users = jdbcTemplate.query(findByUsernameSQL, rowMapper, username);
+        SqlRowSet rs = jdbcTemplate.queryForRowSet(findByUsernameSQL, username);
+
+        User user = new User();
+
+        while (rs.next()){ 
+        user.setUserId(rs.getString("userId")); 
+        user.setUsername(rs.getString("username")); 
+        user.setName(rs.getString("name")); 
+        }
+        
+        return Optional.of(user);
+    }
+
+    public String insertUser(User user){
+
+        String insertSQL = "insert into user (user_id, username) values (?, ?)";
+
+        String genId = UUID.randomUUID().toString().substring(0, 8);
+
+        user.setUserId(genId);
+        
+        jdbcTemplate.update(insertSQL, user.getUserId(), user.getUsername());
+
+        return genId; 
+
+    }
+}
+
+
